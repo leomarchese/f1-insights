8ac25389a02845f27f3e4c3ee5f3269dcc05e7a0
Leonardo Marchese <leo.marchese@hotmail.com>
2025-02-16 13:59:16 -0300

    feat: adding import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@typesApp': path.resolve(__dirname, 'src/types'),
+    },
+  },
 });
